b581f21c091d484e38b53ede1d1e752ec7f145d7
gi12340 <122547815+gi12340@users.noreply.github.com>
2023-11-06 17:20:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+var SpeedRecognition = window.webkitSpeechRecogntion;
+var recogntion = new SpeechRecognition();
+var textbox = document.getElementById("textbox");
+
+function start() 
+{
+    textbox.innerHTML= ""
+    recogntion.start()
+
+}
+recogntion.onresult = function(event){
+    console.log(event)
+    var content = event.results[0][0].transcript
+    textbox.innerHTML= content
+    console.log(content)
+    if(content=="tire minha selfie"){
+        console.log ("tirando selfie")
+        speak()
+    }
+}
+
+function speak(){
+    var synth = window.speechSynthesis
+    speakData = "tirando sua selfie 5 segundos"
+    var utterThis = new SpeechSynthesisUtterance(speakData)
+    synth.speak(utterThis)
+    Webcam.attach(camera)
+    setTimeout(function(){
+        takeSelfie()
+        save()
+    },5000)
+}
+
+camera = document.getelementById("camera")
+Webcam.set({
+    width:360, height:250, image_format:'jpeg', jpeg_quality:90 
+
+})
+
+function takeSelfie()
+{
+    webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML ='<img id="selfieImage" src="'+data_uri+'"/>'
+    })
+}
+
+function save()
+{
+    link = document.getElementById("link")
+    image = document.getElementById("selfieImage").scr
+    link.href = image
+    link.click()
+}
